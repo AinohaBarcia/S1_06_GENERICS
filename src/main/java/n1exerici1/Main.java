22630590afc18bed4e01cb2b3184c319f717dd34
Ainoha Barcia <ainoha.barcia@gmail.com>
2023-11-13 09:41:42 +0100

    Ejercicio corregido

--- src/main/java/n1exerici1/Main.java
+++ src/main/java/n1exerici1/Main.java
@@ -2,16 +2,16 @@ package n1exerici1;
 
 public class Main {
     public static void main(String[] args) {
-        NoGenericMethods <Byte> byteBox = new NoGenericMethods(1,7,3);
-        NoGenericMethods <String> stringBox = new NoGenericMethods<>("Maria","Pedro","Emma");
-        NoGenericMethods <Integer> intBox = new NoGenericMethods<>(122,45,630);
 
-        NoGenericMethods<Object> randomObject = new NoGenericMethods<>(byteBox,intBox,stringBox);
-        NoGenericMethods<Object> randomObject2 = new NoGenericMethods<>(stringBox,byteBox,stringBox);
-        stringBox.setObject1("Pepita");
+        String object1 = "Maria";
+        String object2 = "Pedro";
+        String object3= "Emma";
+        ;
+        NoGenericMethods  ngm = new NoGenericMethods(object1,object2,object3);
 
-        System.out.println(stringBox.getObject1());
-        System.out.println(randomObject);
-        System.out.println(randomObject2);
+        NoGenericMethods ngm2 = new NoGenericMethods(object2,object3,object1);
+
+        System.out.println(ngm);
+        System.out.println(ngm2);
     }
 }
--- src/main/java/n1exerici1/NoGenericMethods.java
+++ src/main/java/n1exerici1/NoGenericMethods.java
@@ -1,46 +1,46 @@
 package n1exerici1;
 
-public class NoGenericMethods <T>{
-    private T object1;
-    private T object2;
-    private T object3;
+public class NoGenericMethods {
+    String object1;
+    String object2;
+    String object3;
 
-    public NoGenericMethods(T object1, T object2, T object3) {
+    public NoGenericMethods( String object1, String object2, String object3) {
         this.object1 = object1;
         this.object2 = object2;
         this.object3 = object3;
     }
 
-    public T getObject1() {
+    public String getObject1() {
         return object1;
     }
 
-    public void setObject1(T object1) {
+    public void setObject1(String object1) {
         this.object1 = object1;
     }
 
-    public T getObject2() {
+    public String getObject2() {
         return object2;
     }
 
-    public void setObject2(T object2) {
+    public void setObject2(String object2) {
         this.object2 = object2;
     }
 
-    public T getObject3() {
+    public String getObject3() {
         return object3;
     }
 
-    public void setObject3(T object3) {
+    public void setObject3(String object3) {
         this.object3 = object3;
     }
 
     @Override
     public String toString() {
         return "NoGenericMethods{" +
-                "object1=" + object1 +
-                ", object2=" + object2 +
-                ", object3=" + object3 +
+                "object1='" + object1 + '\'' +
+                ", object2='" + object2 + '\'' +
+                ", object3='" + object3 + '\'' +
                 '}';
     }
 }
